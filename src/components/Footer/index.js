85c3d9046255e2da6f91dbde1ad039e33b3c7ea1
Tony Phondeth <phondeth@gmail.com>
2021-11-01 22:59:28 -0400

    updated spacing/position

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,30 +3,26 @@ import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 function Footer() {
     return (
-            <footer className="footer">
-                <div>
-                    <a href="https://www.linkedin.com/in/tony-william-phondeth-4478a9202/" target="blank" rel="noopener noreferrer">
-                        <li className="logo">
-                            <FaLinkedin />
-                        </li>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://github.com/TPhondeth" target="blank" rel="noopener noreferrer">
-                        <li className="logo">
-                            <FaGithub />
-                        </li>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://www.instagram.com/" target="blank" rel="noopener noreferrer">
-                        <li className="logo">
-                            <FaInstagram />
-                        </li>
-                    </a>
-                </div>
-            </footer>
-    );
-}
+            <div className="footer">
+                <footer>
+                        <a href="https://www.linkedin.com/in/tony-william-phondeth-4478a9202/" target="blank" rel="noopener noreferrer">
+                            <li className="logo">
+                                <FaLinkedin />
+                            </li>
+                        </a>
+                        <a href="https://github.com/TPhondeth" target="blank" rel="noopener noreferrer">
+                            <li className="logo">
+                                <FaGithub />
+                            </li>
+                        </a>
+                        <a href="https://www.instagram.com/" target="blank" rel="noopener noreferrer">
+                            <li className="logo">
+                                <FaInstagram />
+                            </li>
+                        </a>
+                </footer>
+            </div>
+        );
+    }
 
 export default Footer;
