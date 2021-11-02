8a00066d42d516eb521e2a89e6bfcb3722907df3
Tony Phondeth <phondeth@gmail.com>
2021-11-02 12:08:35 -0400

    updated about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,10 +16,10 @@ function About() {
 			</div>
             <div className="about-me">
                 <p>
-                    Hi my name is Tony! I'm an aspiring web developer based out of Toronto, ON with my small family. My journey into tech started last year when I lost my corporate sales job during the pandemic. I took the opportunity to reinvent myself and pursue a new and challenging career in web development.
+                    Hi my name is Tony. I'm an aspiring web developer based out of Toronto, ON. My journey into tech started last year when I lost my corporate sales job during the pandemic. I took the opportunity to reinvent myself and pursue a new and challenging career in web development.
                 </p>
                 <p>
-                    I'm currently enrolled at the University of Toronto SCS Full-Stack Web Development Bootcamp. The program has helped me build a strong foundation in JavaScript, HTML5, CSS3 and responsive web design. I also possess knowledge in object-oriented programming of progressive web-applications using MongoDB, Express.js, React.js and Node.js (MERN) technology stack. I'm very excited to be able to build creative and innovative applications going forward!
+                    I'm currently enrolled at the University of Toronto SCS Full-Stack Web Development Bootcamp. The program has helped me build a strong foundation in JavaScript, HTML5, CSS3 and responsive web design. I also have knowledge in object-oriented programming of progressive web-applications using MongoDB, Express.js, React.js and Node.js (MERN) technology stack. I've finally found a career that I'm passionate about and am excited to continue learning new technologies and put my skills to use.
                 </p>
             </div>
         </section>
