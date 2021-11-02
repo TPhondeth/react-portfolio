99cb49d63b4b842878e6f89422b864ab2ea049e9
Tony Phondeth <phondeth@gmail.com>
2021-11-02 14:03:11 -0400

    updated h1

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -38,7 +38,7 @@ function ContactForm() {
 
 return (
     <section className="contact">
-        <h1 data-testid="h1tag">Drop me a <i>line!</i></h1>
+        <h1 data-testid="h1tag">Send me an <i>email!</i></h1>
         <form id="contact-form" onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Name:</label>
