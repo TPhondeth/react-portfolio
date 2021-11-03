137b4f0f1329c9340f8a1ff59de49800803f1c10
Tony Phondeth <phondeth@gmail.com>
2021-11-02 20:19:15 -0400

    added page-header

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -9,7 +9,9 @@ import Taskinator from '../../assets/images/taskinator.png';
 function Portfolio() {
     return (
         <section className="portfolio">
-
+        <div className="center">
+            <h1 className="page-header">My Portfolio</h1>
+        </div>
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 g-4 portfolio">
             <div class="col">
                 <div class="card">
