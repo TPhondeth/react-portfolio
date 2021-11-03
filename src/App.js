2793bf95403e26a85cf80dbc7698a7972c1027c8
Tony Phondeth <phondeth@gmail.com>
2021-11-02 21:35:42 -0400

    finalized styling for app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 import Footer from './components/Footer';
 import './App.css';
 
 
 function App() {
+  const [currentTab, setCurrentTab] = useState("about");
+
+  const renderTab = () => {
+    switch (currentTab) {
+      case "about":
+        return <About />;
+      case "portfolio":
+        return <Portfolio />;
+      case "contact":
+        return <Contact />;
+      case "resume":
+        return <Resume />;
+      default:
+        return null;
+    }
+  };
   return (
     <div>
-      <main>
-        <About></About>
+			<div>
+				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+			</div>
+			<div>
+				<main>{renderTab()}</main>
+			</div>
+      <div>
         <Footer></Footer>
-      </main>
-    </div>
+      </div>
+		</div>
   );
 }
 
